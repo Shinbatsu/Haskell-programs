33fb59ee584ca47c9445fc72080563dc24478141
Shinbatsu <nojic132@gmail.com>
2022-08-20 11:06:52 +0300

    fix CONST issue for new GHC version

--- Monads/Monads.hs
+++ Monads/Monads.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 module Monads where
 
-import Prelude hiding (Monad, Identity, Maybe(..), State, Reader, Writer)
 import Data.Monoid
+import Prelude hiding (Identity, Maybe (..), Monad, Reader, State, Writer)
 
 class Monad m where
   return :: a -> m a
   (>>=) :: m a -> (a -> m b) -> m b
 
-data Identity a = Identity a
+newtype Identity a = Identity a
   deriving (Show, Eq)
 
 data Maybe a = Nothing | Just a
@@ -23,17 +25,20 @@ instance Monad Maybe where
   Nothing >>= f = Nothing
   (Just v) >>= f = f v
 
-data State s a = State {runState :: s -> (a, s)}
+newtype State s a = State {runState :: s -> (a, s)}
+
 instance Monad (State s) where
   return a = State $ \s -> (a, s)
   m >>= k = State $ \s -> let (a, s') = runState m s in runState (k a) s'
 
-data Reader s a = Reader {runReader :: s -> a }
-instance Monad (Reader r) where
-  return a = Reader $ \_ -> a
-  m >>= k  = Reader $ \r -> runReader(k (runReader m r)) r
+newtype Reader s a = Reader {runReader :: s -> a}
 
-data Writer w a = Writer {runWriter :: (w, a)}
+instance Monad (Reader r) where
+  return a = Reader $ const a
+  m >>= k = Reader $ \r -> runReader (k (runReader m r)) r
+
+newtype Writer w a = Writer {runWriter :: (w, a)}
+
 instance Monoid w => Monad (Writer w) where
   return val = Writer (mempty, val)
-  (Writer (log, val)) >>= f = Writer(log `mappend` log1, val1) where Writer(log1, val1) = f val
+  (Writer (log, val)) >>= f = Writer (log `mappend` log1, val1) where Writer (log1, val1) = f val
